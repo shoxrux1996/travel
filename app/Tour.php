ba96b61aad0109aec652678c999bd42db42cb264
Shokhrukh <shohrux.shomaxmudov@gmail.com>
2018-01-17 19:44:01 +0500

    new commit

--- app/Tour.php
+++ app/Tour.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use TCG\Voyager\Facades\Voyager;
 
 class Tour extends Model
 {
-
+    public function categories(){
+        return $this->belongsToMany(Voyager::modelClass('Category'));
+    }
 }
